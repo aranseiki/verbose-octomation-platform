76fbabd9058da8a33d7559aa80488709440a1db0
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-08-20 17:22:50 -0300

    Realizado ajuste de no caminho do arquivo a ser executado pelo arquivo principal e ajuste de padronização de código.

--- src/verbose-octomation-platform.py
+++ src/verbose-octomation-platform.py
@@ -4,4 +4,6 @@ from system.config.config_file import DIRETORIO_RAIZ
 
 print(__name__)
 
-resultado_modulo = runpy.run_path(f'{DIRETORIO_RAIZ.parent}/../docs/exemplos/soma.py')
+resultado_modulo = runpy.run_path(
+    f'{DIRETORIO_RAIZ.parent}/../docs/exemplos/soma.py'
+)
