5c298e735a971bd9409cfad8a98f3c84597e493c
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-08-20 17:21:42 -0300

    Realizado ajuste de importação e padronização de código.

--- src/shared/shared_config.py
+++ src/shared/shared_config.py
@@ -1,38 +1,18 @@
 # src/shared/shared_config.py
-import sys
 import os
+import sys
 
 # garante que src esteja no sys.path mesmo quando executado diretamente
 CAMINHO_SRC = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if CAMINHO_SRC not in sys.path:
     sys.path.insert(0, CAMINHO_SRC)
 
-from system.config.config_file import DIRETORIO_RAIZ
+from system.config.config_file import DIRETORIO_RAIZ as DIRETORIO_RPA
+from system.utils.logger_utils import (
+    criar_estrutura_log,
+    localizar_data_hora,
+    registar_log,
+    registar_log_decorator,
+)
 
-print('DIRETORIO_RAIZ:', DIRETORIO_RAIZ)
-
-variavel_shared = ''
-
-
-# import sys, os
-
-# # adiciona o diretório-pai (src), mas só dentro de shared
-# CAMINHO_SYSTEM = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", 'system'))
-# sys.path.append(CAMINHO_SYSTEM)
-# from config.config_file import DIRETORIO_RAIZ as DIRETORIO_RAIZ
-# sys.path.remove(CAMINHO_SYSTEM)
-# del CAMINHO_SYSTEM
-
-
-"""
-def secure_import(from_value: str, import_value: str, as_value: str):
-    CAMINHO_SYSTEM = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", 'system'))
-    sys.path.append(CAMINHO_SYSTEM)
-    from config.config_file import DIRETORIO_RAIZ as DIRETORIO_RAIZ
-    sys.path.remove(CAMINHO_SYSTEM)
-    del CAMINHO_SYSTEM
-    return DIRETORIO_RAIZ
-
-
-DIRETORIO_RAIZ = secure_import(from_value = 'config.config_file', import_value = 'DIRETORIO_RAIZ', as_value = 'DIRETORIO_RAIZ')
-# """
+DIRETORIO_RPA = DIRETORIO_RPA / 'rpa'
