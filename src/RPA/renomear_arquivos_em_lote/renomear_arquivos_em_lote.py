4ab043f05f3588ba8d55b7010d6f24c0a1c77f49
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-08-21 05:49:15 -0300

    Adicionado consumo de configuração das regras de renomear arquivos e coleta dos arquivos do caminho de entrada

--- src/RPA/renomear_arquivos_em_lote/renomear_arquivos_em_lote.py
+++ src/RPA/renomear_arquivos_em_lote/renomear_arquivos_em_lote.py
@@ -6,6 +6,8 @@ if CAMINHO_SRC not in sys.path:
     sys.path.insert(0, CAMINHO_SRC)
 
 from datetime import datetime
+from json import load
+from pathlib import Path
 
 from shared.shared_config import (
     DIRETORIO_RPA,
@@ -14,19 +16,36 @@ from shared.shared_config import (
     registar_log_decorator,
 )
 
-nome_automacao = os.path.basename(__file__).removesuffix('.py')
+NOME_AUTOMACAO = os.path.basename(__file__).removesuffix('.py')
+DIRETORIO_PROJETO = DIRETORIO_RPA / NOME_AUTOMACAO
+DIRETORIO_CONFIG = DIRETORIO_PROJETO / 'config'
+ARQUIVO_CONFIG_RENAME_RULES = DIRETORIO_CONFIG / 'rename_rules.json'
 
 caminho_criado = criar_estrutura_log(
     data_hora=datetime.now(),
     cultura='pt_BR',
-    diretorio_base=DIRETORIO_RPA,
+    diretorio_base=DIRETORIO_PROJETO,
 )
-
-# breakpoint()
 
 registar_log(
     log_level='DEBUG',
-    mensagem=f'Iniciando a automação {nome_automacao}',
+    mensagem=f'Iniciando a automação {NOME_AUTOMACAO}',
     cultura='pt_BR.UTF-8',
     handler_name='root',
 )
+
+with open(ARQUIVO_CONFIG_RENAME_RULES, encoding="utf-8") as arquivo_JSON:
+    arquivo_config_rename_rules_json = load(arquivo_JSON)
+
+# MOCK 
+arquivo_config_rename_rules_json['diretorioEntrada'] = 'C:\\users\\aranseiki\\Downloads'
+# FIM DO MOCK 
+
+lista_arquivos = [
+    item 
+    for item in Path(
+        arquivo_config_rename_rules_json['diretorioEntrada']
+    ).glob('**/*.*')
+    if item.is_file
+]
+
