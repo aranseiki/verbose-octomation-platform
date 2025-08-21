d0a8256b05ec0b46574f46d33524cf4e3399c5a2
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-08-21 16:39:58 -0300

    Adicionado fluxo de identificação e renomeação dos arquivos

--- src/RPA/renomear_arquivos_em_lote/renomear_arquivos_em_lote.py
+++ src/RPA/renomear_arquivos_em_lote/renomear_arquivos_em_lote.py
@@ -12,6 +12,10 @@ from pathlib import Path
 from shared.shared_config import (
     DIRETORIO_RPA,
     criar_estrutura_log,
+    gerar_contador_arquivo,
+    identificar_contador_arquivo,
+    identificar_prefixo_arquivo,
+    identificar_sufixo_arquivo,
     registar_log,
     registar_log_decorator,
 )
@@ -20,6 +24,8 @@ NOME_AUTOMACAO = os.path.basename(__file__).removesuffix('.py')
 DIRETORIO_PROJETO = DIRETORIO_RPA / NOME_AUTOMACAO
 DIRETORIO_CONFIG = DIRETORIO_PROJETO / 'config'
 ARQUIVO_CONFIG_RENAME_RULES = DIRETORIO_CONFIG / 'rename_rules.json'
+SUFIXO_DUPLICADO = 'dup'
+PREFIXO_DUPLICADO = 'dup'
 
 caminho_criado = criar_estrutura_log(
     data_hora=datetime.now(),
@@ -43,10 +49,97 @@ arquivo_config_rename_rules_json[
 ] = 'C:\\users\\aranseiki\\Downloads'
 # FIM DO MOCK
 
+diretorioEntrada = arquivo_config_rename_rules_json['diretorioEntrada']
 lista_arquivos = [
     item
     for item in Path(
-        arquivo_config_rename_rules_json['diretorioEntrada']
+        diretorioEntrada
     ).glob('**/*.*')
     if item.is_file
 ]
+
+modo_ativo_renomeacao = (
+    arquivo_config_rename_rules_json['renomeacao']['modoAtivo']
+)
+parametro_renomeacao = [
+    parametro
+    for parametro
+    in arquivo_config_rename_rules_json['renomeacao']['parametros']
+    if parametro['nome'].upper() == modo_ativo_renomeacao.upper()
+]
+
+if not modo_ativo_renomeacao.upper() in [
+    'PREFIXO',
+    'SUFIXO',
+    'NUMERACAOSEQUENCIAL',
+]:
+    raise ValueError(
+        f'Modo de renomeação {modo_ativo_renomeacao} não implementado.'
+    )
+
+parametro_renomeacao = parametro_renomeacao[0]
+
+for arquivo in lista_arquivos:
+    nome_arquivo = arquivo.stem
+    extensao_arquivo = arquivo.suffix
+    novo_nome_arquivo = ''
+
+    if modo_ativo_renomeacao.upper() == 'PREFIXO':
+        prefixo = str(parametro_renomeacao['valor'])
+        prefixo_arquivo = identificar_prefixo_arquivo(
+            nome_arquivo = nome_arquivo,
+            separador='_',
+        )
+
+        if prefixo_arquivo == prefixo:
+            prefixo = PREFIXO_DUPLICADO
+        elif prefixo_arquivo == PREFIXO_DUPLICADO:
+            prefixo = ''
+
+        if not prefixo == '':
+            prefixo = f'{prefixo}_'
+
+        novo_nome_arquivo = f'{prefixo}{nome_arquivo}'
+    elif modo_ativo_renomeacao.upper() == 'SUFIXO':
+        sufixo = str(parametro_renomeacao['valor'])
+        sufixo_arquivo = identificar_sufixo_arquivo(
+            nome_arquivo = nome_arquivo,
+            separador='_',
+        )
+
+        if sufixo_arquivo == sufixo:
+            sufixo = SUFIXO_DUPLICADO
+        elif sufixo_arquivo == SUFIXO_DUPLICADO:
+            sufixo = ''
+
+        if not sufixo == '':
+            sufixo = f'_{sufixo}'
+
+        novo_nome_arquivo = f'{nome_arquivo}{sufixo}'
+    elif modo_ativo_renomeacao.upper() == 'NUMERACAOSEQUENCIAL':
+        contador = identificar_contador_arquivo(
+            nome_arquivo = nome_arquivo,
+            separador='_',
+        )
+
+        novo_contador = gerar_contador_arquivo(
+            nome_arquivo = nome_arquivo,
+            separador='_',
+        )
+
+        nome_arquivo = nome_arquivo.removesuffix(str(contador))
+        nome_arquivo = nome_arquivo.removesuffix('_')
+
+        contador = novo_contador
+        novo_nome_arquivo = f'{nome_arquivo}_{str(contador)}'
+    
+    arquivo.rename(
+        arquivo.parent / (novo_nome_arquivo + extensao_arquivo)
+    )
+
+registar_log(
+    log_level='DEBUG',
+    mensagem=f'Finalizando a automação {NOME_AUTOMACAO}',
+    cultura='pt_BR.UTF-8',
+    handler_name='root',
+)
