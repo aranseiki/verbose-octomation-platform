18e3dd0275d2136acd8d0b448bd105281b15dcf0
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-08-22 17:29:00 -0300

    movido a função localizar_data_hora para um módulo de utilitários mais apropriado.

---
+++ src/system/utils/toolkit.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def localizar_data_hora(
+    datetime: datetime,
+    cultura: str,
+):
+    import locale
+
+    locale.setlocale(locale.LC_TIME, cultura)
+    data_hora_atual = datetime
+    return data_hora_atual.strftime('%c')
